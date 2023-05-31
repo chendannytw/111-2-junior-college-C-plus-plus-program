92e3c735baf4f0a980d554b39a4fa572b7f31ca2
(1111034023)Ferzbow <87377788+Fertilizer0417@users.noreply.github.com>
2023-05-31 15:55:43 +0800

    把蔡正洋的早餐拿去丟掉

---
+++ 1111034023/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include "Pet.h"// program uses C++ standard string class
+using namespace std;
+
+// GradeBook class definition
+
+
+// function main begins program execution
+int main()
+{
+	Date birth(6, 4, 1989);
+	Date hire(3, 12, 1988);
+	Pet pet("Dog", "BasayBasay", birth, hire);
+
+	cout << endl;
+	pet.print();
+}
